f736673ddc97da381c45d447b14831af10db01de
oretoise <38185128+oretoise@users.noreply.github.com>
2019-05-07 15:22:03 -0500

    Template/Blade Test

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Uhh</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
---
+++ resources/views/programs/test/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.template')
+
+@section('content')
+    <p>Listing of plans</p>
+@endsection
