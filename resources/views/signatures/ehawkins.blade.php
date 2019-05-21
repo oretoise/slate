e6ec07261faaddeb01d864e124b17286978193e6
oretoise <38185128+oretoise@users.noreply.github.com>
2019-05-21 13:53:47 -0500

    Evan and Kali's signature file

--- resources/views/signatures/ehawkins.blade.php
+++ resources/views/signatures/ehawkins.blade.php
@@ -2,5 +2,5 @@
 <i>Online Program Coordinators</i><br/>
 Center for Distance Education<br/>
 Mississippi State University<br/>
-<a href="mailto:e.hawkins@msstate.edu">e.hawkins@msstate.edu</a>
+<a href="mailto:e.hawkins@msstate.edu">e.hawkins@msstate.edu</a><br/>
 <a href="mailto:k.dunlap@msstate.edu">k.dunlap@msstate.edu</a>
