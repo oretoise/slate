343985b2bc3c58678258a67f6500d6140ee7db2f
oretoise <38185128+oretoise@users.noreply.github.com>
2019-05-20 21:25:10 -0500

    Minor BBA update

--- resources/views/programs/bba/home.blade.php
+++ resources/views/programs/bba/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.responsive')
 
 @section('content')
-<h2>{{ strtoupper($program) }} Communication Plan:</h2>
+<h2>BBA Communication Plan:</h2>
 <ul>
     <li><a href="{{ url('bba/zero') }}">zero</a></li>
     <li><a href="{{ url('bba/three') }}">three</a></li>
