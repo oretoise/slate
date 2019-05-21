422ec6a684ef1efef35d3cb889ff6d88b6e2d54f
oretoise <38185128+oretoise@users.noreply.github.com>
2019-05-21 10:52:07 -0500

    SmarterMeasure common file

---
+++ resources/views/common/smartermeasure.blade.php
@@ -0,0 +1,17 @@
+<p>Dear *|FNAME|*,</p>
+
+<p>
+    <img class="img-right" src="https://gallery.mailchimp.com/08645bc06c5353fea3c46c46c/images/a85dcde8-ed1c-4236-aea7-815745dca6c5.png"/>
+    Do you know if you are ready for online learning? If not, you can complete the  <a href="http://distance.msstate.edu/{{ $program }}/ready"><i>SmarterMeasure Learning Readiness Indicator</i></a> assessment. This is a free assessment that can help you determine your strengths and weaknesses related to those of successful distance students based on personality and habits.
+</p>
+
+<p>
+    This assessment does not determine whether you are eligible to take an online course, rather it provides a summary of information that will better assist you in making decisions related to your education. 
+</p>
+<p>
+    You will receive a detailed report of your results to help you better understand your readiness to succeed in online courses once you complete the assessment, too.
+</p>
+
+<p>
+    We look forward to working with you soon!
+</p>
