9d9fd7b71fbe1d17115b914c88420e2200d66a65
oretoise <38185128+oretoise@users.noreply.github.com>
2019-05-20 21:24:35 -0500

    Cleanup

--- resources/views/programs/test/apply.blade.php
+++
@@ -1,2 +0,0 @@
-<a href="https://goo.gl/v2ujcN">
-<img alt="Apply now!" src="https://gallery.mailchimp.com/08645bc06c5353fea3c46c46c/images/4a0dd851-7f19-4b3a-98d5-68fa501de50d.png" title="Apply now!"/></a>
