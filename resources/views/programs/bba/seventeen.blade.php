c40aa723732532b5fa23bbac954997bf82cb55d9
oretoise <38185128+oretoise@users.noreply.github.com>
2019-05-20 21:24:56 -0500

    Update BBA to common UG tuition

--- resources/views/programs/bba/seventeen.blade.php
+++ resources/views/programs/bba/seventeen.blade.php
@@ -2,42 +2,7 @@
 
 @section('content')
 <h3>Day 17 - Subject: How much will your online degree cost?</h3>
-<p>Dear *|FNAME|*,</p>
-<p>
-    <a href="https://youtu.be/_z2sKz5yhwk"><img class="img-right" src="https://goto.msstate.edu/www/images/Distance/Email%20Template/tuition_fees_video.png"/></a>
-    The number one concern for most students is affordability. "How much is this going to cost?"
-</p>
-
-<p>
-    Regardless of your location, <b>ALL</b> online students pay in-state tuition!
-</p>
-
-<p>
-    This short <a href="https://youtu.be/_z2sKz5yhwk">video</a> shows you how to find current cost information.
-</p>
-
-<p>
-    You can also follow these steps to find online tuition and distance fees:
-
-    <ol>
-        <li>Visit the <a href="http://www.controller.msstate.edu/accountservices/tuition/">Tuition/Fees</a> page.</li>
-        <li>Select the <b>Distance Education</b> tab.</li>
-        <li>Refer to the <b>UNDERGRAD</b> table for the semester that you are interested in.</li>
-        <li>Click <b>See Here for Distance Fees</b>.  Please review the current semester fee table to view any additional fees that might be associated with the courses you may take.*</li>
-    </ol>
-</p>
-
-<p>
-    Additional information about <a href="http://distance.msstate.edu/bba/financial">financial aid</a>, <a href="http://distance.msstate.edu/bba/military">military/veteran educational benefits</a> can be found on our <a href="http://distance.msstate.edu/bba">website</a>.
-</p>
-
-<p>
-    If you have any questions after reviewing this information, don’t hesitate to contact me.
-</p>
-
-<p>
-    All the best,
-</p>
+@include('common.undergrad_tuition')
 @endsection
 
 @section('signature')
