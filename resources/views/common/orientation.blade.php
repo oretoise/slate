0bb97af209499ade3f94a05a90f772d61fa41562
oretoise <38185128+oretoise@users.noreply.github.com>
2019-06-10 15:49:13 -0500

    Orientation common file

---
+++ resources/views/common/orientation.blade.php
@@ -0,0 +1,22 @@
+<p>Dear *|FNAME|*,</p>
+
+<p>
+    <img class="img-right" src="https://goto.msstate.edu/www/images/Distance/Email%20Template/orientation.png"/>
+    If you're worried about how you will complete online coursework or how to navigate your courses, don't fret!
+</p>
+
+<p>
+    Once you have been admitted, Mississippi State University offers a non-credit <a href="http://distance.msstate.edu/{{$program}}/orientation"><i>Distance@State Orientation</i></a> course to new distance students at no charge?  This course allows students to become familiar with the online learning process at MSU before their courses actually begin.
+</p>
+
+<p>
+    Of course, I am also here whenever you have a question.
+</p>
+
+<p>
+    I look forward to hearing from you!
+</p>
+
+<p>
+    All the best,
+</p>
