32d78a7e9d3220af69cfad055557fc4a08733ebc
js2506 <js2506@msstate.edu>
2019-07-03 09:30:58 -0500

    More links

--- resources/views/programs/phcl/zero.blade.php
+++ resources/views/programs/phcl/zero.blade.php
@@ -13,11 +13,11 @@
     <li>All distance students pay <b>in-state tuition</b>.</li>
 </ul>
 
-<p>All courses are taught by faculty and staff from MSU.  To see the types of courses that are affiliated with the program, please check out the <a href="http://online.msstate.edu/seed/overview">Course Descriptions</a>.</p>
+<p>All courses are taught by faculty and staff from MSU.  To see the types of courses that are affiliated with the program, please check out the <a href="https://online.msstate.edu/phcl/course-descriptions/">Course Descriptions</a>.</p>
 
 <p>Because MSU is accredited by the Southern Association of Colleges and Schools Commission on Colleges (SACSCOC), your courses will meet the same standards as on-campus courses, and your degree will not differentiate between online or on-campus.</p>
 
-<p>Are you ready to <a href="http://online.msstate.edu/phcl/admissions">apply</a>?  Do you have any questions? I will be glad to assist you.</p>
+<p>Are you ready to <a href="https://online.msstate.edu/phcl/admissions-process">apply</a>?  Do you have any questions? I will be glad to assist you.</p>
 
 <p>Thank you,</p>
 @endsection
