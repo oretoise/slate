195178fa575f65ee5954159f35a28db02edb8b51
oretoise <38185128+oretoise@users.noreply.github.com>
2019-07-30 13:57:44 -0500

    Add view compiler base function

--- app/Http/Controllers/MJML.php
+++ app/Http/Controllers/MJML.php
@@ -47,4 +47,36 @@ class mjml extends Controller
 
         return view($view)->with('program', $program);
     }
+
+    public function compile_view($program, $day = Null) {
+        # TODO: Update this to /programs/...
+        # Use for /tests/test right now
+        if ($day) {
+            $view = $program . "/" . $day;
+        } else {
+            $view = $program . "/home";
+        }
+
+        # Call Blade to render the requested view.
+        $blade = view($view)->with('program', $program)->render();
+
+        # Create an MJML instance.
+        $process = new Process("../node_modules/.bin/mjml --stdin --stdout");
+
+        # Set its STDIN to the result from Blade.
+        $process->setInput($blade);
+
+        # Run MJML.
+        $process->mustRun();
+
+        # Store its output in a variable.
+        $mjml_output = $process->getOutput();
+
+
+        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+        fwrite($myfile, $mjml_output);
+        fclose($myfile);
+
+        echo "done";
+    }
 }
