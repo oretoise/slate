e3911fa72e3c733a8f85ce21fc46e010ab692089
js2506 <js2506@msstate.edu>
2019-08-02 14:44:28 -0500

    Fix "xhttp undefined"

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -38,6 +38,7 @@
 
 <script>
     function compile(plan) {
+        var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState = 4 && this.status == 200) {
                 // Set success message.
