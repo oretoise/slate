558dbf11c4895c64b02e405426aa05d79facd10a
js2506 <js2506@msstate.edu>
2019-08-02 14:15:52 -0500

    Remove old code from app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,44 +52,9 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->fname }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-
-                            @if (! (Auth::user()->admin) && !(Auth::user()->superadmin))
-                                <li class="nav-item">
-
-                                    <form method="post" action="{{ action('TimeController@history') }}" id="history-form">
-                                        @csrf
-                                        <input type="hidden" value="{{ Auth::user()->id }}" name="id">
-                                        <a class="nav-link" onclick="document.getElementById('history-form').submit();">History</a>
-                                    </form>
-                                </li>
-                            @endif
-                        @endguest
+                        <li class="nav-item">
+                            <a class="nav-link" href="/listing">Plans</a>
+                        </li>
                     </ul>
                 </div>
             </div>
