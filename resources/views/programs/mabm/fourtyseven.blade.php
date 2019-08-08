e7048a44d26cd526f12b0974ebcca4df8b88d831
js2506 <js2506@msstate.edu>
2019-08-08 17:04:03 -0500

    MABM Social Media changes

--- resources/views/programs/mabm/fourtyseven.blade.php
+++ resources/views/programs/mabm/fourtyseven.blade.php
@@ -2,7 +2,40 @@
 
 @section('content')
 <h3>Day 47 - Subject: Let's get social!</h3>
-@include('common.social')
+<p>
+    Be part of the Bulldog Family!  Choose your favorite social media platform to connect with us so you can keep up with important deadlines and other topics and essential announcements.
+</p>
+
+<table style="width: 550px; margin: auto;">
+    <tr>
+        <td class="divider social">
+            <a href="https://www.facebook.com/OnlineMABM/">
+                <img src="https://goto.msstate.edu/www/images/Distance/social/facebook_100.png"/>
+            </a>
+        </td>
+        <td class="divider social">
+            <a href="https://twitter.com/online_mabm">
+                <img src="https://goto.msstate.edu/www/images/Distance/social/twitter_100.png" />
+            </a>
+        </td>
+        <td class="divider social">
+            <a href="https://www.instagram.com/mississippistateonline/">
+                <img src="https://goto.msstate.edu/www/images/Distance/social/instagram_100.png" />
+            </a>
+        </td>
+        <td class="divider social">
+            <a href="https://www.linkedin.com/company/mississippi-state-university-center-for-distance-education/?trk=company_name">
+                <img src="https://goto.msstate.edu/www/images/Distance/social/linkedin_100.png" />
+            </a>
+        </td>
+        <td class="divider social">
+            <a href="https://www.youtube.com/channel/UC_rYsq0IfHHR5ysJfLFe9bQ">
+                <img src="https://goto.msstate.edu/www/images/Distance/social/youtube_100.png" />
+            </a>
+        </td>
+    </tr>
+</table>
+<p>Happy following!</p>
 @endsection
 
 @section('signature')
