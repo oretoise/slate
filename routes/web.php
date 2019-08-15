626ceab15be4b5b7aec01a8ca93c078f205c0b69
oretoise <38185128+oretoise@users.noreply.github.com>
2019-08-14 19:16:17 -0500

    New routing

--- routes/web.php
+++ routes/web.php
@@ -14,44 +14,20 @@
 // Homepage. Currently on dev view. TODO: Switch to compiled.
 Route::get('/', 'MJML@dev_view');
 
-// Compiled program views.
-Route::get('/{program}', function($program) {
-    $view = '/programs/' . $program . '/home';
-    return view($view)->with('program', $program);
-});
-Route::get('/{program}/{day}', function($program, $day) {
-    if ($program == 'geosciences') {
-        $view = '/_compiled/geosciences/' . $day . '/home';
-        $program = 'geosciences/' . $day;
-    } else {
-        $view = '/_compiled/' . $program . '/' . $day;
-    }
-    return view($view)->with('program', $program);
-});
+// Compile Route
 
-Route::get('/geosciences/{program}/{day}', function($program, $day) {
-    $view = '/_compiled/geosciences/' . $program . '/' . $day;
-    $program = 'geosciences/' . $program;
-    return view($view)->with('program', $program);
-});
-
-
-
-
-
-// Dev program view.
+// Dev && Geosciences
 Route::get('/dev/geosciences/{program}', 'MJML@dev_view');
-Route::get('/dev/{program}', 'MJML@dev_view');
 Route::get('/dev/geosciences/{program}/{day}', 'MJML@dev_view');
+
+// Dev
+Route::get('/dev/{program}', 'MJML@dev_view');
 Route::get('/dev/{program}/{day}', 'MJML@dev_view');
 
+// Compiled && Geosciences
+Route::get('/geosciences/{program}', 'MJML@compiled');
+Route::get('/geosciences/{program}/{day}', 'MJML@compiled');
 
-
-
-
-
-// TODO: Remove this, but for now it catches anything else.
-Route::get('/{program}', function($program) {
-    $view = '/programs/' . $program . '/home';
-    return view($view)->with('program', $program);
-});
+// Compiled
+Route::get('/{program}', 'MJML@compiled');
+Route::get('/{program}/{day}', 'MJML@compiled');
