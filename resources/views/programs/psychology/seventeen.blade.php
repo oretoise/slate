c8d689230e3a38d1d44e3ab8c6ece2b65881e367
js2506 <js2506@msstate.edu>
2019-08-29 09:48:54 -0500

    Update Psychology Plan

--- resources/views/programs/psychology/seventeen.blade.php
+++ resources/views/programs/psychology/seventeen.blade.php
@@ -29,31 +29,17 @@
             </td>
             <td class="divider" style="text-align: center; width: 50%;">
                 <b>Evan Hawkins</b><br/>
-                <i>BSIS Coordinator/Advisor</i><br/>
+                <i>Coordinator</i><br/>
                 Center for Distance Education<br/>
                 <a href="mailto:e.hawkins@msstate.edu">e.hawkins@msstate.edu</a><br/>
                 (P): 662-325-2655<br/>
                 (F): 622-325-2657
             </td>
         </tr>
-        <tr>
-            <td class="divider" style="width: 50%;">
-				<img src="https://goto.msstate.edu/www/images/Distance/staff/kali_dunlap.png" />
-			</td>
-            <td class="divider" style="text-align: center; width: 50%;">
-                <br/>
-                <b>Kali Dunlap</b><br/>
-                <i>BSIS Coordinator/Advisor</i><br/>
-                Center for Distance Education<br/>
-                <a href="mailto:k.dunlap@msstate.edu">k.dunlap@msstate.edu</a><br/>
-                (P): 662-325-8029<br/>
-                (F): 622-325-2657
-            </td>
-        </tr>
     </tbody>
 </table>
 
-<p>Dr. Eakin, Kali and Evan are here to help.  We look forward to working with you!</p>
+<p>Dr. Eakin and Evan are here to help.  We look forward to working with you!</p>
 @endsection
 
 @section('signature')
