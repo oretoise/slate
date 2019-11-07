83899e6fa27c256891bd366ad099ed5cba5a7bec
Scruggs <js2506@msstate.edu>
2019-11-07 16:40:52 -0600

    BioCert - Days 0-7

--- resources/views/programs/biology/three.blade.php
+++ resources/views/programs/biology/three.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.mjml')
 
 @section('content')
-<h3>Day 3 - Subject: </h3>
+<h3>Day 3 - Subject: Online Graduate General Biology Certificate</h3>
+
+<p>Dear @@{{First}},</p>
+
+<img class="img-right" src="" />
+
+<p>The General Biology Certificate program can be completed in as little as one year (taking two courses per semester). For information pertaining to finances, admissions, course framework, and course information, see the <a href="https://online.msstate.edu/biology/">website</a>.</p>
+
+<p>The general biology degree is designed for high school teachers wanting to teach dual credit courses and for those who would like to teach general biology course at the community college level.  The program includes subjects such as ecology, microbiology, evolutionary biology, and biotechnology.</p>
+
+<p>All coursework, including exams, are submitted online.  Instructional methods utilized:
+	<ul>
+		<li>Video lectures</li>
+		<li>Chat rooms</li>
+		<li>Bulletin boards</li>
+		<li>Online proctoring</li>
+	</ul>
+</p>
+
+<p>Access the <a href="https://online.msstate.edu/biology/program-structure/">program structure</a> on the website to see the types of courses that are available through the General Biology Certificate program.</p>
+<p>Please let me know if I can answer any questions you may have. I'm happy to help!</p>
 @endsection
 
 @section('signature')
--- resources/views/programs/biology/zero.blade.php
+++ resources/views/programs/biology/zero.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 <h3>Day 0 - Subject: Thanks for your interest in the General Biology Certificate!</h3>
+
+<p>Dear @@{{First}},</p>
+
+<p>Thank you for your interest in the online General Biology Certificate program at Mississippi State University (MSU). This program will provide students with a better background and understanding of biological sciences. It is designed to allow teachers to obtain the credentials needed to teach biology-based courses at the community college level or dual enrollment courses at the high school level. Courses are taught completely online by the same professors and instructors who teach main campus biology courses at MSU.</p>
+
+<p>The General Biology Certificate can be earned by completing 18 credit hours of graduate level biology courses and can be completed in one year taking two courses per semester.  The program consists of online courses that span the different disciplines within biology, including ecology, microbiology, evolutionary biology, and biotechnology.</p>
+
+<p>A maximum of six graduate-level and related credit hours may be transferred into the program at the discretion of the graduate coordinator/committee.</p>
+<p>If you are interested in enrolling or would like more information regarding admissions, please refer to the <a href="https://online.msstate.edu/biology/admissions-process">website</a>.</p>
+<p>I hope you will find that the General Biology Certificate program meets your needs and you start the online application for admission.  The admission deadline for the fall is <b>1 August</b>.  If you have any questions or concerns, please contact me at <a href="mailto:joy.bailey@msstate.edu">joy.bailey@msstate.edu</a>, and I will be glad to assist you!</p>
+
+<p>Sincerely,</p>
 @endsection
 
 @section('signature')
