4551e037621f446c5afb3557a76544f0fce6627f
Scruggs <js2506@msstate.edu>
2019-12-02 10:02:58 -0600

    Plan compilation script

---
+++ scripts/compile_plans.py
@@ -0,0 +1,32 @@
+import requests
+import urllib3
+import utilities
+
+from bs4 import BeautifulSoup
+
+# Disable Insecure Request Warning
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def main():
+    # Load host information.
+    host = utilities.get_host_from_env()
+
+    # Load program list.
+    programs_list = utilities.get_program_list()
+
+    for program in programs_list:
+        # Generate URL to request
+        url = host + "/slate/compile/" + program
+
+        print("Requesting", program)
+
+        # Make the request
+        r = requests.get(url)
+
+        # Check result.
+        if str(r.status_code) == '404':
+            print("Error 404 on compile request.")
+
+
+if __name__ == "__main__":
+    main()
