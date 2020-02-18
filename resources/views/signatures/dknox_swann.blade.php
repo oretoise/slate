f046908d6f01b40e59849ab7c497c1bd5283a882
Scruggs <js2506@msstate.edu>
2020-02-18 08:36:34 -0600

    Spacing on Engineering signature file

--- resources/views/signatures/dknox_swann.blade.php
+++ resources/views/signatures/dknox_swann.blade.php
@@ -1,6 +1,6 @@
 <table>
     <tr>
-        <td>
+        <td style="padding-right: 10px; width: 50%;">
             <b><span id="coordinator_name">Daphne Knox</span></b><br/>
             <i>Program Coordinator</i><br/>
             <a href="mailto:daphne.knox@msstate.edu"><span id="coordinator_email">daphne.knox@msstate.edu</span></a>
