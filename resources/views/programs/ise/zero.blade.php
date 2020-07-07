0f6ad107573a6375c1751a919ff95bd7cdf9f1b6
oretoise <38185128+oretoise@users.noreply.github.com>
2020-07-07 11:13:59 -0500

    Update ISE Day 0

--- resources/views/programs/ise/zero.blade.php
+++ resources/views/programs/ise/zero.blade.php
@@ -5,23 +5,38 @@
 
 <p>Dear @@{{First}},</p>
 
+{% if {{program}} == 'PhD Industrial & Systems Engineering' %}
 <p>Thank you for checking out MSU's online Doctor of Philosophy in Industrial & Systems Engineering degree.</p>
+<p>This 68-hour degree program offers 48 hours of coursework with an additional 20 hours of research.  This program requires a preliminary exam, a dissertation, and an oral exam in defense of the dissertation.</p>
+{% endif %}
 
+{% if {{program}} == 'MS in Industrial & Systems Engineering' %}
+<p>Thank you for checking out MSU’s online Master of Science in Industrial Engineering degree.</p>
+<p>This 30-hour degree program has both thesis and non-thesis options.  Students can choose from five concentration track options with their program of study.  These concentrations are:</p>
+<ul>
+	<li>Human Factors and Ergonomics</li>
+	<li>Industrial Systems</li>
+	<li>Management Systems Engineering</li>
+	<li>Manufacturing Systems</li>
+	<li>Operations Research</li>
+</ul>
 
-This 68-hour degree program offers 48 hours of coursework with an additional 20 hours of research.  This program requires a preliminary exam, a dissertation, and an oral exam in defense of the dissertation.
-See how you can get started by:					
-•	Checking out our ISE Graduate Program Overview 
-•	Finding more information with our ISE Graduate Program Guide 
-Application deadlines for online students at Mississippi State are as follows:
-Fall Admission Deadline:  August 1
-Spring Admission Deadline:  December 1
-Summer Admission Deadline:  May 15 
-Because MSU is accredited by the Southern Association of Colleges and Schools Commission on Colleges (SACSCOC) and the Accreditation Board for Engineering and Technology, Inc.(ABET), your courses will meet the same standards as on-campus courses.  Also, your online degree will not be differentiated from an on-campus degree.
-Are you ready to apply? If you have any questions or concerns, please contact us. We are ready to help.
+<p>See how you can get started by:</p>
+<ul>
+	<li>Checking out our <a href="https://www.ise.msstate.edu/academics/graduate/">ISE Graduate Program Overview</a>.</li>
+	<li>Finding more information with our <a href="https://www.ise.msstate.edu/wp-content/uploads/ISE-Graduate-Program-Guide.pdf">ISE Graduate Program Guide</a>.</li>
+</ul>
 
-All the best,
+<p>Application deadlines for online students at Mississippi State are as follows:</p>
+<p>Fall Admission Deadline:  <b>August 1</b><br/>
+Spring Admission Deadline:  <b>December 1</b><br/>
+Summer Admission Deadline:  <b>May 15</b></p>
 
+<p>Because MSU is accredited by the Southern Association of Colleges and Schools Commission on Colleges (SACSCOC) and the Accreditation Board for Engineering and Technology, Inc.(ABET), your courses will meet the same standards as on-campus courses.  Also, your online degree will not be differentiated from an on-campus degree.</p>
 
+<p>Are you <a href="https://online.msstate.edu/ise/admissions-process/">ready to apply</a>? If you have any questions or concerns, please contact us. We are ready to help.</p>
+
+<p>All the best,</p>
 @endsection
 
 @section('signature')
