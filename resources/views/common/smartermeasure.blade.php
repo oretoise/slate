30c4c30661f4cdd363fce8d5282d765e6dc7f6df
Scruggs <js2506@msstate.edu>
2020-07-14 12:40:25 -0500

    Update smartermeasure common email

--- resources/views/common/smartermeasure.blade.php
+++ resources/views/common/smartermeasure.blade.php
@@ -1,15 +1,30 @@
 <p>Dear @@{{First}},</p>
 
 <p>
-    <img class="img-right" src="https://gallery.mailchimp.com/08645bc06c5353fea3c46c46c/images/a85dcde8-ed1c-4236-aea7-815745dca6c5.png"/>
-    Do you know if you are ready for online learning? If not, you can complete the  <a href="https://online.msstate.edu/{{ $program }}/ready-for-online/"><i>SmarterMeasure Learning Readiness Indicator</i></a> assessment. This is a free assessment that can help you determine your strengths and weaknesses related to those of successful distance students based on personality and habits.
+    <img class="img-right" src="https://online.msstate.edu/images/global/elearnready.png"/>
+    Do you know if you are ready for online learning? If not, you can complete the  <a href="https://online.msstate.edu/{{ $program }}/ready-for-online/"><i>eLearnReady Online Education Readiness</i></a> assessment. This is a free assessment that can help you determine your strengths and weaknesses related to those of successful distance students based on personality and habits.
+</p>
+
+<p>eLearnReady is designed to analyze students’ degree of readiness for participating in online courses. Based upon research and statistical analysis, a total of nine salient and measurable factors were identified that include:</p>
+
+<ol>
+    <li>Self-Motivation</li>
+    <li>Self-Management</li>
+    <li>Communication with Instructor</li>
+    <li>Interaction with Peers</li>
+    <li>Learning Preference - Text</li>
+    <li>Learning Preference - Visual</li>
+    <li>Learning Preference - Auditory</li>
+    <li>Technology Skills</li>
+    <li>Classroom Website</li>
+</ol>
+
+<p>
+    At the end of the assessment, you will receive a detailed report of your results to help you better understand your readiness to succeed in online classes.
 </p>
 
 <p>
     This assessment does not determine whether you are eligible to take an online course, rather it provides a summary of information that will better assist you in making decisions related to your education. 
-</p>
-<p>
-    You will receive a detailed report of your results to help you better understand your readiness to succeed in online courses once you complete the assessment, too.
 </p>
 
 <p>
