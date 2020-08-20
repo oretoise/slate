39acc9cfeaf5f6fba4a6bc61f8e5ca89b2372952
Scruggs <js2506@msstate.edu>
2020-08-20 10:45:53 -0500

    Fix CME Day 24

--- resources/views/programs/cme/twentyfour.blade.php
+++ resources/views/programs/cme/twentyfour.blade.php
@@ -3,9 +3,6 @@
 @section('content')
 <h3>Day 24 - Subject: We're here to serve you!</h3>
 @include('common.student_services_plural')
-
-<p>Please let us know if you have any questions. We are happy to help.</p>
-<p>All the best,</p>
 @endsection
 
 @section('signature')
