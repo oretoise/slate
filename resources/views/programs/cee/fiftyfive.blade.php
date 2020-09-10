e3b2d69ca31bf3a43fb080dc462b326fc32248e5
Scruggs <js2506@msstate.edu>
2020-09-10 16:32:47 -0500

    CEE Minor fixes

--- resources/views/programs/cee/fiftyfive.blade.php
+++ resources/views/programs/cee/fiftyfive.blade.php
@@ -13,7 +13,7 @@
 <p>Thank you again for your interest in the Master of Science in Civil Engineering at Mississippi State Online. We just wanted to touch base with you again to see if you had any follow up questions.</p>
 {% endif %}
 
-<img class="img-center" width=750px src="https://goto.msstate.edu/www/images/Distance/programs/cme/cme_2.jpg" />
+<img class="img-center" width=750px src="https://goto.msstate.edu/www/images/Distance/programs/cee/cee_2.png" />
 
 {% if @@{{program}} == 'Master of Science in Civil Engineering' %}
 <p>You may start the Master of Science in Civil Engineering program at the beginning of the fall, spring or summer semester.  The sooner you complete your <a href="https://apply.grad.msstate.edu/">application</a>, the closer you are to reaching your educational goal!</p>
