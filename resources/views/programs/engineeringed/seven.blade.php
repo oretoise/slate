b2004d6be256dde2ad358d51930be392b2e28564
Scruggs <js2506@msstate.edu>
2020-09-10 12:59:36 -0500

    PhD Engr Ed - Content for all

--- resources/views/programs/engineeringed/seven.blade.php
+++ resources/views/programs/engineeringed/seven.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.mjml')
 
 @section('content')
-<h3>Day 7 - Subject: </h3>
+<h3>Day 7 - Subject: Start your application today!</h3>
+
+<a href="https://youtu.be/-gzq528lad0"><img class="img-right" src="https://goto.msstate.edu/www/images/Distance/Email%20Template/grad_admissions_video.png" /></a>
+
+<p>Dear @@{{First}},</p>
+<p>Start your application for the online Doctor of Philosophy in Engineering with a concentration in Engineering Education program today!</p>
+
+<p>Remember to choose <span style="color: #5D1725;"><b>Doctor of Philosophy in Engineering with a concentration in Engineering Education</b></span> as your Major, and <span style="color: #5D1725;"><b>Engineering Education</b></span> and <span style="color: #5D1725;"><b>Online Education</b></span> as your campus when applying for <a href="https://online.msstate.edu/engineeringed/admissions-process/">admission</a> or <a href="https://online.msstate.edu/engineeringed/admissions-process/">readmission</a>.  
+
+<p>If you have any questions, just ask. We are happy to help.</p>
+<p>Thank you for considering Mississippi State for your online degree.</p>
+<p>All the best,</p>
 @endsection
 
 @section('signature')
--- resources/views/programs/engineeringed/three.blade.php
+++ resources/views/programs/engineeringed/three.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.mjml')
 
 @section('content')
-<h3>Day 3 - Subject: </h3>
+<h3>Day 3 - Subject: If you have questions, we have answers!</h3>
+
+<p>Dear @@{{First}},</p>
+
+<p>Wondering if the time to start is now?  Here are some answers to frequently asked questions to help you confidently take the next step.</p>
+
+<ul>
+	<li>
+		<span style="color: #5D1725;"><b>Do you accept transfer credit?</b></span><br/>
+		Yes, you may transfer up to 12 hours from an accredited university. A <a href="https://www.grad.msstate.edu/files/transfer_approval_form.pdf">formal transfer approval form</a> must be submitted to receive credit.<br/><br/>
+	</li>
+	<li>
+		<span style="color: #5D1725;"><b>Can the entire program be completed online?</b></span><br/>
+		Yes, the coursework can be completed 100% online.
+	</li>
+</ul>
+
+<p>If you have other questions or if we can assist you in any way, please contact us.  We are ready to help.</p>
+<p>All the best,</p>
 @endsection
 
 @section('signature')
