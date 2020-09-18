73b1b7e3f7e1186e8f59d9c3f10406cbe52508be
oretoise <38185128+oretoise@users.noreply.github.com>
2020-09-18 10:57:10 -0500

    Conservation - Content

--- resources/views/programs/conservation/ten.blade.php
+++ resources/views/programs/conservation/ten.blade.php
@@ -1,7 +1,36 @@
 @extends('layouts.mjml')
 
 @section('content')
-<h3>Day 10 - Subject: </h3>
+<h3>Day 10 - Subject: Meet your MS Conservation Education team!</h3>
+
+<p>Dear @@{{First}},</p>
+
+<p>At Mississippi State University, you have a team of staff who will support you in your pursuit of your degree.</p>
+
+<table>
+    <tbody>
+        <tr>
+            <td class="divider staff">
+                <img src="https://goto.msstate.edu/www/images/Distance/staff/daphne_knox.png">
+            </td>
+            <td class="divider staff" style="text-align: left;">
+                <p>Daphne Knox has been working for the Center for Distance Education at Mississippi State University since 2018.  She coordinates programming for the College of Engineering, College of Forest Resources, and the College of Agriculture and Life Sciences.</p>
+                <p>Daphne holds a Bachelor of Arts in Political Science and a Master of Public Policy and Administration from Mississippi State University.  In her leisure time, Daphne enjoys spending time with her family, playing tennis, and reading.</p>
+            </td>
+        </tr>
+		<tr>
+            <td class="divider staff">
+                <img src="https://goto.msstate.edu/www/images/Distance/programs/conservation/stacy_hines.jpg">
+            </td>
+            <td class="divider staff" style="text-align: left;">
+                <p>Dr. Stacy L. Hines is an instructor in the Department of Wildlife, Fisheries and Aquaculture at Mississippi State University. She currently has appointments in teaching and program coordination for the online Conservation Education program. Her background in nonformal and formal conservation education are central to her work.  She currently serves as a board member for the Friends of the Noxubee Refuge and on the Mississippi Environmental Education Alliance.</p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<p>We look forward to working with you!</p>
+<p>All the best,</p>
 @endsection
 
 @section('signature')
--- resources/views/programs/conservation/three.blade.php
+++ resources/views/programs/conservation/three.blade.php
@@ -1,7 +1,34 @@
 @extends('layouts.mjml')
 
 @section('content')
-<h3>Day 3 - Subject: </h3>
+<h3>Day 3 - Subject: If you have questions, we have answers!</h3>
+<p>Dear @@{{First}},</p>
+
+<p>Wondering if the time to start is now?  Here are some answers to frequently asked questions to help you confidently take the next step.</p>
+
+<ul>
+	<li>
+		<span style="color: #5D1725;"><b>Who Should apply?</b></span><br/>
+		The Conservation Education non-thesis master’s program prepares you to become a leader in the field of conservation education. The program is designed both for full and part-time students who are interested in enhancing conservation-focused educational, scientific, and leadership skills.<br/><br/>
+	</li>
+	<li>
+		<span style="color: #5D1725;"><b>Why choose MSU’s Conservation Education program?</b></span><br/>
+		<b>Mississippi State Online’s non-thesis master’s degree in Conservation Education gives you the opportunity to:</b><br/>
+		<ul>
+			<li>Complete the only Conservation Education program in the United States that is 100% online with a focus on advancing leaders in conservation education.</li>
+			<li>This program was designed by our faculty to meet accreditation and industry standards.  The conservation education faculty are scientific researchers with experience in both non-formal and formal educational settings.</li>
+			<li>Work and take classes. Designed for the working professional, our classes are fully online with weekly tasks assigned for you to complete on your schedule.</li>
+		</ul>
+		<br/><br/>
+	</li>
+	<li>
+		<span style="color: #5D1725;"><b>Can the entire program be completed online?</b></span><br/>
+		Yes, the coursework can be completed 100% online. This program is asynchronous and requires a minimum of 30 credit hours for completion.
+	</li>
+</ul>
+
+<p>If you have other questions or if we can assist you in any way, please contact us.  We are ready to help.</p>
+<p>All the best,</p>
 @endsection
 
 @section('signature')
