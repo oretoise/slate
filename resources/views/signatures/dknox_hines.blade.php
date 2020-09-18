98aa333c3d971e15b4f1f9869f4b1fdba72b43ef
oretoise <38185128+oretoise@users.noreply.github.com>
2020-09-18 10:37:01 -0500

    Daphne and Stacy's signature for MS Conservation

---
+++ resources/views/signatures/dknox_hines.blade.php
@@ -0,0 +1,14 @@
+<table>
+    <tr>
+        <td style="padding-right: 10px; width: 50%;">
+            <b><span id="coordinator_name">Daphne Knox</span></b><br/>
+            <i>Program Coordinator</i><br/>
+            <a href="mailto:daphne.knox@msstate.edu"><span id="coordinator_email">daphne.knox@msstate.edu</span></a>
+        </td>
+        <td>
+            <b>Stacy L. Hines</b><br/>
+            <i>Instructor, Coordinator for Distance Education Programming</i><br/>
+            <a href="mailto:sla335@msstate.edu">sla335@msstate.edu</a>
+        </td>
+    </tr>
+</table>
