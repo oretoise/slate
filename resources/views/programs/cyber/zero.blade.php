681d333027f5e3dd04480a6edcbd91b3fb3618df
Jordan Scruggs <js2506@msstate.edu>
2021-03-15 11:34:45 -0500

    Cyber - Content

--- resources/views/programs/cyber/zero.blade.php
+++ resources/views/programs/cyber/zero.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <h3>Day 0 - Subject: Thank you for checking out MSU's online Master of Science in Cyber Security and Operations program!</h3>
 
-<img class="img-right" width="350px" src="https://goto.msstate.edu/www/images/Distance/programs/cse/cse_1.png" />
+<img class="img-right" width="350px" src="https://goto.msstate.edu/www/images/Distance/programs/cyber/cyber_1.png" />
 
 <p>Dear @@{{First}},</p>
 <p>This 31-credit hour degree has both thesis and non-thesis options. The Master of Science in Cyber Security and Operations is designed for students who wish to help meet the challenges posed by increasing cyber-threats. Using a multidisciplinary approach, the program is designed to provide students with a focused education within a broad analytical framework for evaluating, understand, and solving cyber security problems.   Recognized nationally for its leadership in cyber security, MSU is one of only nine schools in the U.S. to hold all three of the National Security Agency’s centers of academic excellence credentials: CAE-Cyber Defense Education, CAE-Cyber Defense Research, and CAE-Cyber Operations.</p>
