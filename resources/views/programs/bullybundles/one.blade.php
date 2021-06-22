41709d8792cf118c3aaeb7d13b4f2f3df251e926
Jordan Scruggs <jordan.d.scruggs@gmail.com>
2021-06-22 15:31:22 -0500

    Add first and second to bullybundles

---
+++ resources/views/programs/bullybundles/one.blade.php
@@ -0,0 +1,69 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title></title>
+    <style type="text/css">@media only screen and (max-width: 480px) { table, img { width: 100% !important; height: auto !important; } }
+    </style>
+  </head>
+  <body>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:650px;">
+      <tbody>
+        <tr>
+          <td>
+          <p>
+            <span style="font-family:Times New Roman,Times,serif;"><img alt="" src="https://goto.msstate.edu/www/images/Distance/Application/email%20-1.png" style="width: 750px; height: 422px;" /></span>
+          </p>
+
+          <div>
+            <span style="font-family:Times New Roman,Times,serif;"><span style="font-size:16px;">Hey, @@{{Preferred}}!</span><span style="font-size:11pt"><span style="line-height:normal"><span style="font-family:&quot;Calibri&quot;,sans-serif"><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"></span></span></span></span></span></span><br />
+            <br />
+            <span style="font-size:11pt"><span style="line-height:normal"><span style="font-family:&quot;Calibri&quot;,sans-serif"><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Are you thinking about earning your degree online?&nbsp; Summer 2021 is the perfect time to start! If you complete your Summer 2021 <b></b>application by <b>Friday, February 26, 2021</b> you will be eligible to earn BullyBundles!&nbsp; Click</span></span><a href="https://www.apply.msstate.edu/" style="color:blue; text-decoration:underline"><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">here</span></span></a><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">to apply.</span></span></span></span></span><br />
+            <span style="font-size:11pt"><span style="line-height:normal"><span style="font-family:&quot;Calibri&quot;,sans-serif"><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><br />
+            <b><span style="color:#660000">What is a BullyBundle?</span></b><br />
+            A BullyBundle is an assortment of Mississippi State Online items including pens, tech items, and more!<br />
+            <br />
+            <b><span style="color:#660000">How do I earn a BullyBundle?</span></b></span></span></span></span></span>
+
+            <ol>
+              <li style="margin-bottom:11px">
+                <span style="font-size:11pt"><span style="line-height:normal"><span style="tab-stops:list .5in"><span style="font-family:&quot;Calibri&quot;,sans-serif"><a href="http://www.apply.msstate.edu/" style="color:blue; text-decoration:underline" target="_blank"><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><span style="color:#660000">Apply for admission</span></span></span></a><b>&nbsp;</b><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">by <b>Friday, February 26, 2021</b> for the Summer 2021 semester. Once you have completed your application, you'll get your first BullyBundle!&nbsp;<br />
+                &nbsp;</span></span></span></span></span></span>
+              </li>
+              <li style="margin-bottom:11px">
+                <span style="font-size:11pt"><span style="line-height:normal"><span style="tab-stops:list .5in"><span style="font-family:&quot;Calibri&quot;,sans-serif"><b><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><span style="color:#660000">Get admitted!</span></span></span></b><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">Once you have been accepted as a Mississippi State University student, you'll get your second BullyBundle!<br />
+                &nbsp;</span></span></span></span></span></span>
+              </li>
+              <li style="margin-bottom:11px">
+                <span style="font-size:11pt"><span style="line-height:normal"><span style="tab-stops:list .5in"><span style="font-family:&quot;Calibri&quot;,sans-serif"><b><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><span style="color:#660000">Enroll</span></span></span></b><b></b><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">in <b>Summer 2021</b> courses! Once enrolled in courses for the <b>Summer 2021</b> semester, you'll receive your final BullyBundle after the semester begins!</span></span></span></span></span></span>
+              </li>
+            </ol>
+            <span style="font-size:11pt"><span style="line-height:normal"><span style="font-family:&quot;Calibri&quot;,sans-serif"><b><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><span style="color:#660000">When will I get my BullyBundle?</span></span></span></b><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><br />
+            BullyBundles will be mailed out the week the first week of March.&nbsp; &nbsp;<br />
+            <br />
+            <i>*Only those seeking an online undergraduate or graduate degree who apply for Summer 2021 admission through</i><b>Friday, February 26, 2021</b><i>will be eligible for BullyBundles.</i><br />
+            <br />
+            We look forward to helping you achieve your educational goals!&nbsp;<br />
+            If you have any questions, please email them to&nbsp;</span></span><a href="mailto:mindy.wolfe@msstate.edu" style="color:blue; text-decoration:underline"><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">mindy.wolfe@msstate.edu</span></span></a><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">.</span></span></span></span></span><br />
+            <span style="font-family:Times New Roman,Times,serif;"><br />
+            <strong>Mindy Wolfe</strong><br />
+            <em>Recruitment Coordinator</em><br />
+            <a href="mailto:mindy.wolfe@msstate.edu">mindy.wolfe@msstate.edu</a><br />
+            Mississippi State University Online<br />
+            <br />
+            Follow us on social media! #msstateonline | #hailstatefromanystate<br />
+            <br />
+            <img alt="" src="https://goto.msstate.edu/www/images/Distance/msstate_online_horizontal.png" style="width: 624px; height: 87px;" /></span>
+
+            <p class="x_MsoNormal" style="margin: 0in 0in 0.0001pt; font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial;">
+              <span style="font-family:Times New Roman,Times,serif;"><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">FB:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://www.facebook.com/MississippiStateUniversityOnline/" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">Mississippi State University Online</a><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">&nbsp;TW:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://twitter.com/msstateonline" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">@msstateonline</a><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">IG:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://www.instagram.com/mississippistateonline/?hl=en" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">@mississippistateonline</a>&nbsp;<span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">LI:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://www.linkedin.com/company/mississippi-state-university-center-for-distance-education/?viewAsMember=true" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">Mississippi State University Online</a><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">&nbsp;YT:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://www.youtube.com/channel/UC_rYsq0IfHHR5ysJfLFe9bQ?view_as=subscriber" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">Mississippi State University Online</a><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);"></span></span>
+            </p>
+
+            <p class="x_MsoNormal" style="margin: 0in 0in 0.0001pt; font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial;">
+              <span style="font-family:Times New Roman,Times,serif;"></span>
+            </p>
+          </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
---
+++ resources/views/programs/bullybundles/two.blade.php
@@ -0,0 +1,49 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title></title>
+    <style type="text/css">@media only screen and (max-width: 480px) { table, img { width: 100% !important; height: auto !important; } }
+    </style>
+  </head>
+  <body>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:650px;">
+      <tbody>
+        <tr>
+          <td>
+          <p>
+            <span style="font-family:Times New Roman,Times,serif;"><img alt="" src="https://goto.msstate.edu/www/images/Distance/Application/email%20-1.png" style="width: 750px; height: 422px;" /></span>
+          </p>
+
+          <div>
+            <p>
+              <span style="font-family:Times New Roman,Times,serif;"><span style="font-size:16px;">Hey, @@{{First}}!</span></span><span style="font-size:11pt"><span style="line-height:normal"><span style="font-family:&quot;Calibri&quot;,sans-serif"><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"></span></span></span></span></span>
+            </p>
+
+            <p style="color: rgb(0, 0, 0); font-family: &quot;Times New Roman&quot;; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;">
+              <span style="font-size:11pt"><span style="line-height:normal"><span style="font-family:&quot;Calibri&quot;,sans-serif"><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif">We are ready for you to become a MSU Bulldog!&nbsp; <b>Summer 2021</b> is the perfect time to start!&nbsp; Enroll for <b>Summer 2021</b> by <b>April 23, 2021,</b> and you are eligible to earn a BullyBundle!&nbsp; If you haven’t received an email about how to enroll please let me know.</span></span></span></span></span><br />
+              <span style="font-size:11pt"><span style="line-height:normal"><span style="font-family:&quot;Calibri&quot;,sans-serif"><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"><br />
+              We look forward to helping you achieve your educational goals!<br />
+              <br />
+              If you have any questions, just respond to this email, and I will be happy to help you!</span></span><span style="font-size:12.0pt"><span style="font-family:&quot;Times New Roman&quot;,serif"></span></span></span></span></span>
+            </p>
+            <span style="font-family:Times New Roman,Times,serif;"><strong>Mindy Wolfe</strong><br />
+            <em>Coordinator</em><br />
+            Center for Distance Education<br />
+            Mississippi State University &nbsp;<br />
+            Phone: 662.325.<strong>8545<br />
+            <br />
+            <span style="font-size:11.0pt"><span style="font-family:&quot;Calibri&quot;,sans-serif"><img src="https://goto.msstate.edu/www/images/Distance/msstate_online_logo.png" style="height:68px; width:146px" /></span></span></strong></span>
+
+            <p class="x_MsoNormal" style="margin: 0in 0in 0.0001pt; font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial;">
+              <span style="font-family:Times New Roman,Times,serif;"><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);"></span></span>
+            </p>
+
+            <p class="x_MsoNormal" style="margin: 0in 0in 0.0001pt; font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial;">
+              <span style="font-family:Times New Roman,Times,serif;"></span>
+            </p>
+          </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
