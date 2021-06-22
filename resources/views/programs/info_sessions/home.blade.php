6e918949868a965d5777d60059c9bcb16b8fb1f7
Jordan Scruggs <jordan.d.scruggs@gmail.com>
2021-06-22 14:03:48 -0500

    Example Info Session Mailing

--- resources/views/programs/info_sessions/home.blade.php
+++ resources/views/programs/info_sessions/home.blade.php
@@ -5,6 +5,7 @@
 <ul>
 	<li>
 		<a href="{{ url('info_sessions/two') }}">Day 2 Email (All Communication Plans)</a>
+		<a href="{{ url('info_sessions/example') }}">Example Announcement/Invitation Email</a>
 	</li>
 </ul>
 @endsection
