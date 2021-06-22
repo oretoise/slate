0b8442964a8346ead302fc1ba0dba71b2d011c8f
Jordan Scruggs <jordan.d.scruggs@gmail.com>
2021-06-22 14:19:11 -0500

    Incomplete apps reminder

---
+++ resources/views/programs/incomplete_apps/example.blade.php
@@ -0,0 +1,36 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title></title>
+    <style type="text/css">@media only screen and (max-width: 480px) { table, img { width: 100% !important; height: auto !important; } }
+    </style>
+  </head>
+  <body>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:650px;">
+      <tbody>
+        <tr>
+          <td>
+          <p>
+            <span style="font-family:Times New Roman,Times,serif;"><img alt="" src="https://goto.msstate.edu/www/images/Distance/Application/email%20-1.png" style="width: 750px; height: 422px;" /></span>
+          </p>
+
+          <div>
+            <span style="font-size:16px;">Hey @@{{Preferred}},</span>
+
+            <p class="xxmsonormal">
+              <span style="font-size:12.0pt">Thank you for starting an application with Mississippi State University Online! I see that your application is still awaiting submission.&nbsp; Once your <a href="https://apply.msstate.edu">application</a> is completed and payment has been submitted, we can begin the review process.&nbsp; If you have any questions about the application process, please reach out. Have a great day!</span>
+            </p>
+            <span style="font-size:16px;">Thank you,<br />
+            <br />
+            <strong>Ashley Wilson</strong><br />
+            <em>Program Facilitator</em><br />
+            Center for Distance Education<br />
+            Mississippi State University</span><br />
+            Phone: 662.325.1559<br />
+            <br />
+          </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
---
+++ resources/views/programs/incomplete_apps/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.mjml')
+
+@section('content')
+<h3>Info Session Mailings</h3>
+<ul>
+	<li>
+		<a href="{{ url('incomplete_apps/example') }}">Incomplete Materials Reminder Example</a>
+	</li>
+</ul>
+@endsection
