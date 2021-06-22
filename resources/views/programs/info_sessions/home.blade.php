884679db5ecc8121a99d37c83e21fdae61d1b8d3
Jordan Scruggs <jordan.d.scruggs@gmail.com>
2021-06-22 13:55:25 -0500

    Info Sessions - directory and day 2

---
+++ resources/views/programs/info_sessions/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.mjml')
+
+@section('content')
+<h3>Info Session Mailings</h3>
+<ul>
+	<li>
+		<a href="{{ url('info_sessions/two') }}">Day 2 Email (All Communication Plans)</a>
+	</li>
+</ul>
+@endsection
---
+++ resources/views/programs/info_sessions/two.blade.php
@@ -0,0 +1,52 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title></title>
+    <style type="text/css">@media only screen and (max-width: 480px) { table, img { width: 100% !important; height: auto !important; } }
+    </style>
+  </head>
+  <body>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" style="width:650px;">
+      <tbody>
+        <tr>
+          <td>
+          <p>
+            <span style="font-family:Times New Roman,Times,serif;"><img alt="" src="https://goto.msstate.edu/www/images/Distance/Application/email%20-1.png" style="width: 750px; height: 422px;" /></span>
+          </p>
+
+          <div>
+            <span style="font-family:Times New Roman,Times,serif;"><span style="font-size:16px;">Hi {{Preferred}}!</span></span>
+
+            <p style="color: rgb(0, 0, 0); font-family: &quot;Times New Roman&quot;; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-style: initial; text-decoration-color: initial;">
+              MSU Online offers virtual information sessions to all prospective students!<br />
+              <br />
+              The information sessions will provide you with information about admissions requirements, details about online orientation, and insights into our services and resources. I will also be there to answer any questions you may have. You don’t want to miss it!<br />
+              <br />
+              Are you interested in registering?&nbsp; If so, please view the session options at the <a href="https://goto.msstate.edu/register/cde_info_sessions?person={{person-id}}">MSU Online registration page</a>.<br />
+              &nbsp;<br />
+              <strong>If you have any questions about the information sessions or would like specific degree information, please reach out to me.</strong><br />
+              <br />
+              Hail State!
+            </p>
+            <span style="font-family:Times New Roman,Times,serif;"><strong>Mindy Wolfe</strong><br />
+            <em>Recruitment Coordinator</em><br />
+            <a href="mailto:mindy.wolfe@msstate.edu">mindy.wolfe@msstate.edu</a><br />
+            Mississippi State University Online<br />
+            <br />
+            Follow us on social media! #msstateonline | #hailstatefromanystate<br />
+            <br />
+            <img alt="" src="https://goto.msstate.edu/www/images/Distance/msstate_online_horizontal.png" style="width: 624px; height: 87px;" /></span>
+
+            <p class="x_MsoNormal" style="margin: 0in 0in 0.0001pt; font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial;">
+              <span style="font-family:Times New Roman,Times,serif;"><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">FB:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://www.facebook.com/MississippiStateUniversityOnline/" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">Mississippi State University Online</a><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">&nbsp;TW:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://twitter.com/msstateonline" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">@msstateonline</a><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">IG:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://www.instagram.com/mississippistateonline/?hl=en" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">@mississippistateonline</a>&nbsp;<span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">LI:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://www.linkedin.com/company/mississippi-state-university-center-for-distance-education/?viewAsMember=true" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">Mississippi State University Online</a><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);">&nbsp;YT:&nbsp;</span><a data-auth="NotApplicable" data-ogsc="" href="https://www.youtube.com/channel/UC_rYsq0IfHHR5ysJfLFe9bQ?view_as=subscriber" rel="noopener noreferrer" style="margin: 0px; padding: 0px; border: 0px; font: inherit; vertical-align: baseline; color: rgb(5, 99, 193); text-decoration: underline;" target="_blank">Mississippi State University Online</a><span data-ogsc="rgb(102, 0, 0)" style="margin: 0px; padding: 0px; border: 0px none; font-style: inherit; font-variant: inherit; font-weight: inherit; font-stretch: inherit; font-size: inherit; line-height: inherit; font-size-adjust: inherit; font-kerning: inherit; font-optical-sizing: inherit; font-language-override: inherit; font-feature-settings: inherit; font-variation-settings: inherit; vertical-align: baseline; color: rgb(102, 0, 0);"></span></span>
+            </p>
+
+            <p class="x_MsoNormal" style="margin: 0in 0in 0.0001pt; font-size: 11pt; font-family: Calibri, sans-serif; color: rgb(0, 0, 0); font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-style: initial; text-decoration-color: initial;">
+              <span style="font-family:Times New Roman,Times,serif;"></span>
+            </p>
+          </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
