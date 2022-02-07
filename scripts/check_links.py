173a13039d96d7ecfdd7358ffa79a6d036ddb796
Jordan Scruggs <js2506@msstate.edu>
2022-02-07 11:50:59 -0600

    Update check_links.py

--- scripts/check_links.py
+++ scripts/check_links.py
@@ -2,12 +2,17 @@ import requests
 import urllib3
 import utilities
 
+import pandas as pd
+
 from bs4 import BeautifulSoup
 
 # Disable Insecure Request Warning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def main():
+    # Make dataframe to hold links.
+    df = pd.DataFrame()
+
     # Load host information.
     host = utilities.get_host_from_env()
     
@@ -23,7 +28,7 @@ def main():
             
             # Split the filename into just the day number and generate URL.
             email = email.split('.')[0]
-            url = host + "/slate/" + program + "/" + email
+            url = host + program + "/" + email
             
             # Make the request.
             r = requests.get(url)
@@ -33,29 +38,43 @@ def main():
 
             for link in soup.findAll('a'):
                 if link.has_attr('href'):
+
+                    # Ignore mailto links.
                     if "mailto" in link['href']:
                         continue
                     else:
 
+                        # Look for our links.
                         if 'online.msstate.edu' in link['href'] or 'distance.msstate.edu' in link['href']:
-                            problem = False
+                            insecure = False
+                            outdated = False
+                            not_found = False
 
                             if 'http://' in link['href']:
-                                print("INSECURE")
-                                problem = True
+                                insecure = True
                             
                             if 'distance.msstate.edu' in link['href']:
-                                print("OUTDATED")
-                                problem = True
-                            
-                            if problem:
-                                print(link['href'])
+                                outdated = True
                             
                             # req
                             check = requests.get(link['href'], verify=False)
                             if str(check.status_code) == '404':
-                                print("404:", link['href'])
+                                not_found = True
+                            
+                            link_dict = {
+                                "href": link["href"],
+                                "insecure": insecure,
+                                "outdated": outdated,
+                                "not_found": not_found,
+                                "email": email,
+                                "program": program
+                            }
 
+                            df = df.append(link_dict, ignore_index=True)
+
+    
+    df = df.drop_duplicates(subset=['href'])
+    df.to_csv("links.csv", index=False)
 
 if __name__ == "__main__":
     main()
