298eb295edf9b18a62b11e8220618ad81c1d47c5
Jordan Scruggs <jordan.d.scruggs@gmail.com>
2022-02-23 12:52:35 -0600

    MSCYSO 0 - Add structure link

--- resources/views/programs/mscyso/zero.blade.php
+++ resources/views/programs/mscyso/zero.blade.php
@@ -10,7 +10,7 @@
 
 <p>See how you can get started by:</p>
 <ul>
-	<li>Reviewing the program structure.</li>
+	<li>Reviewing the <a href="https://online.msstate.edu/mscyso#program-structure">program structure</a>.</li>
 	<li>Requesting information about your desired area of study.</li>
 </ul>
 
