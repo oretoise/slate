4c8e2c36f24e3cf59bc659c38a30eff4614c44c9
Jordan Scruggs <js2506@msstate.edu>
2022-02-24 16:54:45 -0600

    CEE Split - Content

--- resources/views/programs/mscee/fiftyfive.blade.php
+++ resources/views/programs/mscee/fiftyfive.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.mjml')
 
 @section('content')
-<h3>Day 55 - Subject: </h3>
+<h3>Day 55 - Subject: The Bulldog family wants you!</h3>
+
+<p>Dear @@{{First}},</p>
+<p>Thank you again for your interest in the Master of Science in Civil Engineering at Mississippi State Online. We just wanted to touch base with you again to see if you had any follow up questions.</p>
+<img class="img-center" width=750px src="https://goto.msstate.edu/www/images/Distance/programs/cee/cee_2.png" />
+<p>You may start the Master of Science in Civil Engineering program at the beginning of the fall, spring or summer semester.  The sooner you complete your <a href="https://apply.grad.msstate.edu/">application</a>, the closer you are to reaching your educational goal!</p>
+<p>All the best,</p>
 @endsection
 
 @section('signature')
--- resources/views/programs/mscee/seven.blade.php
+++ resources/views/programs/mscee/seven.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.mjml')
 
 @section('content')
-<h3>Day 7 - Subject: </h3>
+<h3>Day 7 - Subject: Start your application today!</h3>
+
+<a href="https://youtu.be/-gzq528lad0" title="Your Guide to Graduate Admissions"><img class="img-right" src="https://goto.msstate.edu/www/images/Distance/Email%20Template/grad_admissions_video.png" /></a>
+
+<p>Dear @@{{First}},</p>
+
+
+<p>Start your application for the online Master of Science in Civil Engineering program.  Here is a short video to help you navigate the application process.</p>
+
+<p>Remember to choose <span style="color: #5D1725;"><b>Master of Science</b></span> and <span style="color: #5D1725;"><b>Civil Engineering</b></span> as your Major and <span style="color: #5D1725;"><b>Online Education</b></span> as your campus when applying for <a href="https://online.msstate.edu/mscee#admissions-process/">admission or readmission</a>.</p>
+
+<p>All the best,</p>
 @endsection
 
 @section('signature')
