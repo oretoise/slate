ea84f96d1201ab1d858b32c389461e52025c299b
oretoise <jordan.d.scruggs@gmail.com>
2022-03-07 15:27:22 -0600

    Update deadlines for engineering

--- resources/views/programs/ise/zero.blade.php
+++ resources/views/programs/ise/zero.blade.php
@@ -30,9 +30,9 @@
 </ul>
 
 <p>Application deadlines for online students at Mississippi State are as follows:</p>
-<p>Fall Admission Deadline:  <b>August 1</b><br/>
-Spring Admission Deadline:  <b>December 1</b><br/>
-Summer Admission Deadline:  <b>May 15</b></p>
+<p>Fall Admission Deadline:  <b>June 1</b><br/>
+Spring Admission Deadline:  <b>November 1</b><br/>
+Summer Admission Deadline:  <b>May 1</b></p>
 
 <p>Because MSU is accredited by the Southern Association of Colleges and Schools Commission on Colleges (SACSCOC) and the Accreditation Board for Engineering and Technology, Inc.(ABET), your courses will meet the same standards as on-campus courses.  Also, your online degree will not be differentiated from an on-campus degree.</p>
 
--- resources/views/programs/phase/zero.blade.php
+++ resources/views/programs/phase/zero.blade.php
@@ -16,9 +16,9 @@
 </ul>
 
 <p>Application deadlines for online students at Mississippi State are as follows:</p>
-<p>Fall Admission Deadline:  <b>August 1</b><br/>
-Spring Admission Deadline:  <b>December 1</b><br/>
-Summer Admission Deadline:  <b>May 15</b></p>
+<p>Fall Admission Deadline:  <b>June 1</b><br/>
+Spring Admission Deadline:  <b>November 1</b><br/>
+Summer Admission Deadline:  <b>May 1</b></p>
 <p>Because MSU is accredited by the Southern Association of Colleges and Schools Commission on Colleges (SACSCOC) and the Accreditation Board for Engineering and Technology, Inc. (ABET), your courses will meet the same standards as on-campus courses.  Also, your online degree will not be differentiated from an on-campus degree.</p>
 <p>Are you <a href="https://online.msstate.edu/phase#admissions-process">ready to apply</a>? If you have any questions or concerns, please contact us. We are ready to help.</p>
 <p>All the best,</p>
