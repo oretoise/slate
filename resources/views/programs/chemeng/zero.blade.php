852c59c4591334248107ba045b5041731eda2fcb
oretoise <jordan.d.scruggs@gmail.com>
2022-03-07 15:06:01 -0600

    Chemical Engineering App Deadlines

--- resources/views/programs/chemeng/zero.blade.php
+++ resources/views/programs/chemeng/zero.blade.php
@@ -30,9 +30,19 @@
 // end MS
 
 <p>Application deadlines for online students at Mississippi State are as follows:</p>
-<p>Fall Admission Deadline:  <b>August 1</b><br/>
-Spring Admission Deadline:  <b>December 1</b><br/>
-Summer Admission Deadline:  <b>May 15</b></p>
+<p><b>For Domestic Students:</b></p>
+<ul>
+	<li>Spring Enrollments need applications by <b>October 15th</b></li>
+    <li>Summer Enrollments need applications by <b>March 15th</b></li>
+    <li>Fall Enrollments need applications by <b>June 15th</b></li>
+</ul>
+
+<p><b>For International Students:</b></p>
+<ul>
+	<li>Spring Enrollments need applications by <b>August 15th</b></li>
+	<li>Summer Enrollments need applications by <b>February 15th</b></li>
+	<li>Fall Enrollments need applications by <b>April 15th</b></li>
+</ul>
 
 <p>Because MSU is accredited by the Southern Association of Colleges and Schools Commission on Colleges (SACSCOC) and the Accreditation Board for Engineering and Technology, Inc.(ABET), your courses will meet the same standards as on-campus courses.  Also, your online degree will not be differentiated from an on-campus degree.</p>
 <p>Are you <a href="https://online.msstate.edu/chemeng/admissions-process/">ready to apply</a>? If you have any questions or concerns, please contact us. We are ready to help.</p>
