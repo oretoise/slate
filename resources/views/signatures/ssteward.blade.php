644f78dbca5a6dfb907f66bfbcc38b1dc1da521a
Jordan Scruggs <js2506@msstate.edu>
2022-03-28 15:36:07 -0500

    Fix susan's signature

--- resources/views/signatures/ssteward.blade.php
+++ resources/views/signatures/ssteward.blade.php
@@ -1,4 +1,11 @@
-<b>Susan Steward</b><br/>
+<b><span id="coordinator_name">Samantha Clardy</span></b><br/>
+<i>Online Program Coordinator</i><br/>
+Center for Distance Education<br/>
+Mississippi State University<br/>
+662.325.9092<br/>
+<a href="mailto:sam103@msstate.edu"><span id="coordinator_email">sam103@msstate.edu</span></a>
+
+<b><span id="coordinator_name">Susan Steward</span></b><br/>
 <i>MAT Coordinator</i><br/>
 662.325.0527<br/>
-<a href="mailto:ssteward@colled.msstate.edu">ssteward@colled.msstate.edu</a>
+<a href="mailto:ssteward@colled.msstate.edu"><span id="coordinator_email">ssteward@colled.msstate.edu</span></a>
