cebb24f6223ec0d9840813bc428bc7c4a6fa4bff
Michael Garlotte <michaeljgarlotteii@gmail.com>
2022-08-08 09:31:21 -0500

    error

--- resources/views/programs/athlete/seventeen.blade.php
+++ resources/views/programs/athlete/seventeen.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <h3>Day 17 - Subject: How much will my online degree cost?</h3>
-@include('common.grad_tuition)
+@include('common.grad_tuition')
 @endsection
 
 @section('signature')
