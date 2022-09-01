37a64b6730a3309d73340fc3005b8498c3c06dec
Michael Garlotte <michaeljgarlotteii@gmail.com>
2022-09-01 15:39:43 -0500

    final edits

--- resources/views/programs/datascience/seven.blade.php
+++ resources/views/programs/datascience/seven.blade.php
@@ -4,7 +4,7 @@
 <h3>Day 7 - Subject: Start your application today!</h3>
 <p>Dear @@{{First}},</p>
 
-<p>Start your application for the online Doctor of Philosophy in Engineering with a concentration in Computer Science program.  Here is a short <a href="https://youtu.be/YQIXYYITnZs">video</a> to help you navigate the application process.</p>
+<p>Start your application for the online Data Science Pedagogy Certificate.  Here is a short <a href="https://youtu.be/YQIXYYITnZs">video</a> to help you navigate the application process.</p>
 
 
 <p>Remember to choose <span style="color: #5D1725;"><b>Data Science Pedagogy Certificate</b></span> as your program of interest and <span style="color: #5D1725;"><b>Online Education</b></span> as your campus when applying for <a href="https://online.msstate.edu/admissions-process/">admission</a> or <a href="https://online.msstate.edu/admissions-process/">readmission</a>.</p>
@@ -16,7 +16,7 @@
 
 @section('signature')
 	@include('signatures.dknox')
-@endsection
+@endsection	
 
 @section('apply')
 	@include('common.apply')
